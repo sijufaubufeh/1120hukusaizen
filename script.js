40a6858cb4e60e50fd286d8f56989350f11c956c
sijufaubufeh <emu.shxx@gmail.com>
2024-11-30 05:56:30 +0900

    script.js を更新

--- script.js
+++ script.js
@@ -7,16 +7,13 @@ window.onload = function() {
     setInterval(updateTime, 1000);
     updateTime();
 };
-function disableScroll() {
-    document.documentElement.style.overflow = 'hidden'; // htmlのスクロールを無効化
-    document.body.style.overflow = 'hidden'; // bodyのスクロールを無効化
-}
+// circle-container内でスクロールイベントを禁止
+const circleContainer = document.querySelector('.circle-container');
 
-function enableScroll() {
-    document.documentElement.style.overflow = ''; // デフォルトに戻す
-    document.body.style.overflow = '';
-}
+circleContainer.addEventListener('wheel', (event) => {
+    event.preventDefault(); // ホイールスクロールを無効化
+});
 
-// 実行例
-disableScroll(); // スクロールを無効化
-enableScroll();  // スクロールを有効化
+circleContainer.addEventListener('touchmove', (event) => {
+    event.preventDefault(); // タッチスクロールを無効化
+}, { passive: false });
